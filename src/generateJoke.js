cb239569b230ce2c279ef8e2fbd5a4b11b1d24ce
rashmin66 <hansi20ra@gmail.com>
2023-03-07 16:14:49 +0530

    add things

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,4 +1,15 @@
-function generateJoke(){
-    return "I don't trust stairs. They're always up to something."
+import axios from 'axios'
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  axios.get('https://icanhazdadjoke.com', config).then((res) => {
+    document.getElementById('joke').innerHTML = res.data.joke
+  })
 }
+
 export default generateJoke
